4211a4239b752dc35971804b24a99c13af2e8170
Natasa Kasikovic <nkasikovic@yahoo.com>
2025-09-18 15:27:23 +0200

    Initial commit

---
+++ backend/src/main/java/com/security/pki/PkiApplication.java
@@ -0,0 +1,12 @@
+package com.security.pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PkiApplication.class, args);
+  }
+}
