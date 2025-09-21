342c12fecc291a62fc85c5d2ef6360a5b566a90d
Lazar Nagulov <lazarnagulov1@gmail.com>
2025-09-21 17:48:50 +0200

    feat: Add security boilerplate and logger

--- backend/src/main/java/com/security/pki/config/SecurityConfig.java
+++ backend/src/main/java/com/security/pki/config/SecurityConfig.java
@@ -1,27 +1,59 @@
 package com.security.pki.config;
 
+import com.security.pki.security.auth.CustomAccessDeniedHandler;
+import com.security.pki.security.auth.RestAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-    /**
-     * NOTE: This SecurityFilterChain is currently configured for testing purposes only.
-     * All routes are permitted without authentication.
-     * In the future, this configuration will be replaced with proper role-based access control.
-     */
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                .cors(Customizer.withDefaults())
+                .exceptionHandling(e -> e
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                        .authenticationEntryPoint(restAuthenticationEntryPoint)
                 )
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable);
+
 
         return http.build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/login")
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/registration")
+                .requestMatchers(HttpMethod.GET, "/", "/webjars/*", "/*.html", "favicon.ico",
+                        "/*/*.html", "/*/*.css", "/*/*.js");
+
+    }
+
 }
---
+++ backend/src/main/java/com/security/pki/shared/services/LoggerService.java
@@ -0,0 +1,38 @@
+package com.security.pki.shared.services;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.LogRecord;
+
+@Service
+public class LoggerService {
+    private static final Logger logger = Logger.getLogger(LoggerService.class.getName());
+    private final FileHandler fileHandler= new FileHandler("log%g.log",15000000,40,true);
+
+    public LoggerService() throws IOException {
+        this.init();
+    }
+
+    public void init(){
+        logger.addHandler(fileHandler);
+    }
+
+    public void withRecord(LogRecord log){
+        logger.log(log);
+    }
+
+    public void info(String log){
+        logger.info(log);
+    }
+
+    public void warning(String log){
+        logger.warning(log);
+    }
+
+    public void severe(String log){
+        logger.severe(log);
+    }
+}
