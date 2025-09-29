cc2f935b4f58f9118ac06d1ab69607d3b851135e
Ana Šinik <sinikana7@gmail.com>
2025-09-29 19:19:55 +0200

    feat: add InvalidCsrException for CSR parsing errors

--- backend/src/main/java/com/security/pki/certificate/handlers/CertificateHandlers.java
+++ backend/src/main/java/com/security/pki/certificate/handlers/CertificateHandlers.java
@@ -3,6 +3,7 @@ package com.security.pki.certificate.handlers;
 import com.security.pki.certificate.exceptions.CertificateCreationException;
 import com.security.pki.certificate.exceptions.CertificateDownloadException;
 import com.security.pki.certificate.exceptions.CertificateNotAllowedToSignException;
+import com.security.pki.certificate.exceptions.InvalidCsrException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +22,13 @@ public class CertificateHandlers {
     public ResponseEntity<String> handleCertificateNotAllowedToSignException(RuntimeException ex) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCsrException.class)
+    public ResponseEntity<String> handleInvalidCsr(InvalidCsrException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
 }
--- backend/src/main/java/com/security/pki/certificate/services/CSRService.java
+++ backend/src/main/java/com/security/pki/certificate/services/CSRService.java
@@ -2,12 +2,10 @@ package com.security.pki.certificate.services;
 
 import com.security.pki.auth.services.AuthService;
 import com.security.pki.certificate.dtos.CertificateRequestDto;
+import com.security.pki.certificate.exceptions.InvalidCsrException;
 import com.security.pki.certificate.mappers.CertificateMapper;
 import com.security.pki.user.models.User;
 import lombok.RequiredArgsConstructor;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.x500.RDN;
-import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
@@ -16,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStreamReader;
 import java.security.PublicKey;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -39,21 +36,27 @@ public class CSRService {
             // certificateService.createEndEntityCertificate(request, publicKey);
             System.out.println("Certificate request: " + request);
         } catch (Exception ex) {
-            throw new RuntimeException("CSR processing failed: " + ex.getMessage(), ex);
+            throw new InvalidCsrException("Failed to upload CSR file");
         }
     }
 
-    private PKCS10CertificationRequest loadCsr(MultipartFile file) throws Exception {
+    private PKCS10CertificationRequest loadCsr(MultipartFile file) {
         try (PEMParser parser = new PEMParser(new InputStreamReader(file.getInputStream()))) {
             Object parsed = parser.readObject();
-            if (parsed instanceof PKCS10CertificationRequest csr) {
+            if (parsed instanceof PKCS10CertificationRequest csr)
                 return csr;
-            }
-            throw new IllegalArgumentException("Provided file is not a valid CSR");
+
+            throw new InvalidCsrException("Provided file is not a valid CSR");
+        } catch (Exception e) {
+            throw new InvalidCsrException("Failed to parse CSR file");
         }
     }
 
-    private PublicKey extractKey(PKCS10CertificationRequest csr) throws Exception {
-        return new JcaPEMKeyConverter().getPublicKey(csr.getSubjectPublicKeyInfo());
+    private PublicKey extractKey(PKCS10CertificationRequest csr) {
+        try {
+            return new JcaPEMKeyConverter().getPublicKey(csr.getSubjectPublicKeyInfo());
+        } catch (Exception e) {
+            throw new InvalidCsrException("Failed to extract public key from csr");
+        }
     }
 }
