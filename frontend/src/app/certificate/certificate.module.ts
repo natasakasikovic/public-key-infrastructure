d948be25ab79843ade7abb131e4fc0b59dd229bc
Ana Šinik <sinikana7@gmail.com>
2025-09-29 11:50:33 +0200

    feat: add form for uploading .pem file and selecting signing CA certificate

--- frontend/src/app/certificate/certificate.module.ts
+++ frontend/src/app/certificate/certificate.module.ts
@@ -3,18 +3,10 @@ import { CommonModule } from '@angular/common';
 import {MaterialModule} from '../infrastructure/material/material.module';
 import {CertificateOverviewComponent} from './certificate-overview/certificate-overview.component';
 import {RootCertificateIssuanceComponent} from './root-certificate-issuance/root-certificate-issuance.component';
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef,
-  MatHeaderRow,
-  MatRow, MatTable
-} from '@angular/material/table';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CertificateDetailsComponent } from './certificate-details/certificate-details.component';
 import {RouterLink} from '@angular/router';
+import { CsrSelfUploadComponent } from './csr-self-upload/csr-self-upload.component';
 
 
 
@@ -22,7 +14,8 @@ import {RouterLink} from '@angular/router';
   declarations: [
     CertificateOverviewComponent,
     RootCertificateIssuanceComponent,
-    CertificateDetailsComponent
+    CertificateDetailsComponent,
+    CsrSelfUploadComponent
   ],
   imports: [
     CommonModule,
